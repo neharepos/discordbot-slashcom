9535e8677b859979f3860343de99f42c4349f825
neharepos <nehakeshrimail@gmail.com>
2024-04-26 21:44:40 +0530

    adding fun to use slash commands

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const { Client, GatewayIntentBits } = require ("discord.js");
+const dotenv = require ("dotenv");
+dotenv.config()
+
 
 const client = new Client({ 
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessage] 
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
 });
 
 
-client.on('messageCreate', message => {
-    console.log
-})
+client.on("messageCreate", (message) => {
+    if (message.author.bot) return;
+    message.reply({
+        content: "Hi From Bot",
+    });
+});
+
+client.login(process.env.TOKEN);
